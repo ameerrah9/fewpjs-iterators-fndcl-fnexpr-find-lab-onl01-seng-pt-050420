2ea0d30a4c4ee63f7f9d03e1dae7e9dd716073c3
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-11-27 17:54:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 superbowlWin = (record) => {
   let result = record.find( record => record.result === "W" )
   return !!result ? result.year : undefined
+// condition ? expression1 : expression2;
+  // if result
+  //   return result.year
+  // else
+  //   return undefined
+  // end
 }
